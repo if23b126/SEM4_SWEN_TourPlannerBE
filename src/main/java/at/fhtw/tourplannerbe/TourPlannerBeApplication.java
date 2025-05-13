3155e029754d6c27a4f9be39f946de70b59ea1d6
Christian Eder <c.eder@aon.at>
2025-05-13 13:44:00 +0200

    Initial Commit

---
+++ src/main/java/at/fhtw/tourplannerbe/TourPlannerBeApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.tourplannerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPlannerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourPlannerBeApplication.class, args);
+    }
+
+}
